802936b7c7894dccce71bbb644bb4e3189f086ef
Vanderson Campanholi <vando.zc@gmail.com>
2018-07-08 11:50:16 -0300

    Adjust front

--- src/main/webapp/app/cliente/emprestimo/emprestimo.controller.js
+++ src/main/webapp/app/cliente/emprestimo/emprestimo.controller.js
@@ -13,14 +13,14 @@
   function EmprestimoController(ClienteService, $state, $stateParams) {
 
       var vm = this;
-      vm.cadastro = {};
+      vm.cliente = {};
 
       vm.simularEmprestimo = simularEmprestimo;
 
       if ($stateParams.id) {
         ClienteService.findById($stateParams.id)
           .then(function (data) {
-              vm.cadastro = data;
+              vm.cliente = data;
           });
       }
 
